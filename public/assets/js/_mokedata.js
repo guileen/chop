54b37bcafb4270083789f434138a409508d7a5f8
finscn <fins@163.com>
2012-05-06 06:07:49 +0800

    update ui

--- public/assets/js/_mokedata.js
+++ public/assets/js/_mokedata.js
@@ -1,48 +1,48 @@
 var mock={
 
 	'topic' : [
-		{
-			id : "1",
-			title : "Topic-1"
-		},
-		{
-			id : "2",
-			title : "Topic-2"
-		},
-		{
-			id : "3",
-			title : "Topic-2"
-		},
-		{
-			id : "4",
-			title : "Topic-2"
-		},
-		{
-			id : "5",
-			title : "Topic-2"
-		}
+		// {
+		// 	id : "1",
+		// 	title : "Topic-1"
+		// },
+		// {
+		// 	id : "2",
+		// 	title : "Topic-2"
+		// },
+		// {
+		// 	id : "3",
+		// 	title : "Topic-2"
+		// },
+		// {
+		// 	id : "4",
+		// 	title : "Topic-2"
+		// },
+		// {
+		// 	id : "5",
+		// 	title : "Topic-2"
+		// }
 
 	],
 
 	'message' : [
-		{
-			topicid : "1" ,
-			sender : "Tom",
-			datetime : "2011-01-20",
-			text : "qwe asd zxc"
-		},
-		{
-			topicid : "1" ,
-			sender : "TomWWWW",
-			datetime : "2011-06-20",
-			text : "qwe asd zxc"
-		},
-		{
-			topicid : "2" ,
-			sender : "Jim",
-			datetime : "2011-08-20",
-			text : "zxc zxc zxc"
-		}
+		// {
+		// 	topicid : "1" ,
+		// 	sender : "Tom",
+		// 	datetime : "2011-01-20",
+		// 	text : "qwe asd zxc"
+		// },
+		// {
+		// 	topicid : "1" ,
+		// 	sender : "TomWWWW",
+		// 	datetime : "2011-06-20",
+		// 	text : "qwe asd zxc"
+		// },
+		// {
+		// 	topicid : "2" ,
+		// 	sender : "Jim",
+		// 	datetime : "2011-08-20",
+		// 	text : "zxc zxc zxc"
+		// }
 
 	]
 
